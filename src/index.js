84b9d5e6877bcc16163e3eb5dc447ba58a4b05c7
Hamza Elaasemy <hamza.elaasemy@imedia24.de>
2020-10-05 14:20:50 +0100

    configurating Firebase'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Firebase, { FirebaseContext } from './firebase';
 import * as serviceWorker from './serviceWorker';
 import {
   BrowserRouter as Router,
@@ -11,7 +12,9 @@ class Root extends React.Component {
  
   render() {
     return (
+      <FirebaseContext.Provider value={new Firebase()}>
      <App/>
+     </FirebaseContext.Provider>
     );
   }
 }
